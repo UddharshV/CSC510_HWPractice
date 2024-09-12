707af2449267c13e0a51f3c659d46e49388f26fe
Uddharsh Vasili <Uddharsh.v19@gmail.com>
2024-09-12 06:58:55 +0000

    Updated test_sample.py to increase code coverage

--- test_sample.py
+++ test_sample.py
@@ -3,3 +3,5 @@ def test_answer1():
     assert myfile.recursive_expo(2,3)==8
 def test_answer2():
     assert myfile.recursive_expo(2,3)==9
+def test_answer3():
+    assert myfile.recursive_expo(4,2)==16
